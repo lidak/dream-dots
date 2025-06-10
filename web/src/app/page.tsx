a46c591ef3a6507f07d967780567b83818c19e59
lidak <animatio.dev@gmail.com>
2025-06-10 18:51:35 -0500

    theme colors added. Header component in progress

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,11 @@
+import { PageLayout } from "@/components/pageLayout";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-start justify-items-top min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center">
+    <PageLayout>
+    <div className="grid grid-rows-[1fr_20px] items-start justify-items-top min-h-screen p-8 pb-2 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] items-center">
         <h1 className="p-10" style={{fontSize: 30}}>Thanks for stopping by!!!</h1>
         <Image
           src="/logo.png"
@@ -17,9 +19,7 @@ export default function Home() {
           <h2>The website is under construction. No kidding! The logo is almost ready and one small story will be published soon! So stay tuned.</h2>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        All rights are reserved by the Little artist ;)
-      </footer>
     </div>
+    </PageLayout>
   );
 }
