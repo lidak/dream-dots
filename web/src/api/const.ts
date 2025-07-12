0864ec09123b9b2944d5e91efabb6850ab918650
lidak <animatio.dev@gmail.com>
2025-07-11 21:11:31 -0500

    CartIcon styling fixed. New BE url updated

--- web/src/api/const.ts
+++ web/src/api/const.ts
@@ -1,4 +1,4 @@
-export const baseUrl = 'https://fermsp2ah6.execute-api.us-west-2.amazonaws.com/Prod';
+export const baseUrl = 'https://bvvzj4c6gl.execute-api.us-west-2.amazonaws.com/Prod';
 import { xApiKey } from '../../../env.json';
 export const commonHeaders = {
   'x-api-key': xApiKey,
