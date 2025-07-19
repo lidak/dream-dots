8cfe83f463a7acdce3b4895a0a6c3906bd038c7f
lidak <animatio.dev@gmail.com>
2025-07-18 22:20:57 -0500

    added api config for dev

--- web/src/api/const.ts
+++ web/src/api/const.ts
@@ -1,6 +1,9 @@
-export const baseUrl = 'https://bvvzj4c6gl.execute-api.us-west-2.amazonaws.com/Prod';
+const isProd = process.env.NODE_ENV === 'production';
+
+export const baseUrl = isProd ? 'https://bvvzj4c6gl.execute-api.us-west-2.amazonaws.com/Prod': 'https://4czhygoo62.execute-api.us-west-2.amazonaws.com/Dev';
+
 import { xApiKey } from '../../../env.json';
 export const commonHeaders = {
-  'x-api-key': xApiKey,
+  'x-api-key': isProd? xApiKey.prod : xApiKey.dev,
   'Content-Type': 'application/json',
 };
